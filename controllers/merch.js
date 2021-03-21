eec0e3adc076ed84951b29da292e63c5a728d8d8
Nicholas <nickycrandall@gmail.com>
2021-03-21 18:08:26 -0400

    added merch seeding and finished index page

--- controllers/merch.js
+++ controllers/merch.js
@@ -17,4 +17,37 @@ router.get('/', (req, res) => {
     })
  })
 
+//merch seeding
+router.get('/seed', (req, res) => {
+    Merch.create([
+        {
+            name: "Last Gen Games Coffee Mug",
+            description: "Love our store? Get your own LGG coffee mug to support us! Dishwasher safe.",
+            img: "mug.png",
+            price: 10,
+            qty: 30,
+        },
+        {
+            name: "Last Gen Games T-shirt",
+            description: "Our LGG shirts are made with 100% cotton.  Machine washable, made in the USA.",
+            img: "shirt.png",
+            price: 20,
+            qty: 15,
+        },
+        {
+            name: "Last Gen Games GameCube Controller",
+            description: "Includes SSBM notches for shield dropping and firefox angles. Guaranteed to have good dashback even without UCF.",
+            img: "gcc.png",
+            price: 245,
+            qty: 2,
+        },
+    ], (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect('/merch')
+    })
+})
+
+
 module.exports = router;
